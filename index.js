3556d896f0af3ad6f5ae6eebc8f55b26a02e5cbd
laverock <they.all.alike@gmail.com>
2019-10-31 11:01:18 +0300

    fix form

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 
 // Campground.create(
 //     {
-//         name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=788&q=80'    }, (err, campground) => {
+//         name: 'Granite Hill',
+//         image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=788&q=80',
+//         description: 'This is a huge granite hill. No bathrooms. No water. Beautiful granite.'
+//     }, (err, campground) => {
 //     if (err) {
 //         console.log(err);
 //     } else {
@@ -37,7 +41,7 @@ app.get('/campgrounds', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds', { campgrounds: allCampgrounds });
+            res.render('index', { campgrounds: allCampgrounds });
         }
     })
 });
@@ -45,7 +49,8 @@ app.get('/campgrounds', (req, res) => {
 app.post('/campgrounds', (req, res) => {
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image: image}
+    const description = req.body.description;
+    const newCampground = {name: name, image: image, description: description};
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
             console.log(err);
@@ -57,6 +62,18 @@ app.post('/campgrounds', (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
+});
+
+app.get('/campgrounds/:id', (req, res) => {
+    // find the campground with provided id
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // and render show page with that campground
+            res.render('show', {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, () => {
