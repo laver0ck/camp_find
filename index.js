abb7c9f2b55cad1e577626f397c724be6e815bb2
laverock <they.all.alike@gmail.com>
2019-12-07 18:41:51 +0300

    add flash messages with bootstrap

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express           = require('express'),
       app               = express(),
       bodyParser        = require('body-parser'),
       mongoose          = require('mongoose'),
+      flash             = require('connect-flash'),
       passport          = require('passport'),
       LocalStrategy     = require('passport-local'),
       methodOverride    = require('method-override'),
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use(flash());
 
 // seedDB(); // seed the database
 
@@ -41,6 +43,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
     next();
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -45,6 +45,7 @@ router.post('/login', passport.authenticate('local', {
 // logout route
 router.get('/logout', (req, res) => {
     req.logout();
+    req.flash('success', 'Logged you out!');
     res.redirect('/campgrounds');
 });
 
